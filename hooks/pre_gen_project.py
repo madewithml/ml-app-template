ea7f0d2caf9a4bbbaa848b1e09ef4706c863f2e8
Goku Mohandas <gokumd@gmail.com>
2018-12-24 12:25:06 -0700

    fixed cookiecutter json

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,11 +1,2 @@
 import os
-import sys
-import re
-
-SLUG_REGEX = r'^[a-z0-9]+(?:-[a-z0-9]+)*$'
-service_name = "{{ cookiecutter.service_name }}"
-
-if not re.match(SLUG_REGEX, service_name):
-    print("Error: {0} is not a valid slug format".format(service_name))
-    sys.exit(1)
 
